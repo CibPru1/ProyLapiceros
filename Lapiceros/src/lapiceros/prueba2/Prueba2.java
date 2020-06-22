f3729d066344bda8ab96e269048dea805e42601f
ja2o <jobispo10@outlook.com>
2020-06-22 18:00:29 -0500

    Usuario 2 haciendo commits de pruebas

---
+++ Lapiceros/src/lapiceros/prueba2/Prueba2.java
@@ -0,0 +1,10 @@
+package lapiceros.prueba2;
+
+public class Prueba2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Nueva clase. Ojala funcione");
+	}
+
+}
--- Lapiceros/src/lapiceros/pruebas/PrincipalLapiceros.java
+++ Lapiceros/src/lapiceros/pruebas/PrincipalLapiceros.java
@@ -4,6 +4,8 @@ public class PrincipalLapiceros {
 
 	public static void main(String[] args) {
 		System.out.println("Hola qué tal desde Escenario1");
+		System.out.println("Esto funciona por ahora");
+		
 	}
 
 }
