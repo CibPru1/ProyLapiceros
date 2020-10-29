17eb8c877efaa143f4f43a9e92518cc44bd78c28
ja2o <jaimeobispo31@gmail.com>
2020-10-29 18:53:16 -0500

    haciendo pruebas de subida

--- Lapiceros/src/lapiceros/prueba2/Prueba2.java
+++ Lapiceros/src/lapiceros/prueba2/Prueba2.java
@@ -5,6 +5,10 @@ public class Prueba2 {
 	public static void main(String[] args) {
 		
 		System.out.println("Nueva clase. Ojala funcione");
+		System.out.println("Esto es desde lo nuevo");
+		System.out.println("Esto es desde lo nuevo");
+		System.out.println("Esto es desde lo nuevo");
+		
 	}
 
 }
